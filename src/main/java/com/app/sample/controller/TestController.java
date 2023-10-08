174e10f8031790af94add13e5866c4360cc1d883
ranji <ranji@Ranjith>
2023-10-08 21:32:18 +0530

    Loop to form Json to convert into yaml

--- src/main/java/com/app/sample/controller/TestController.java
+++ src/main/java/com/app/sample/controller/TestController.java
@@ -3,6 +3,8 @@ package com.app.sample.controller;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,75 +20,95 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 public class TestController {
 
 	String yamlSchemaProp = "";
-	String yamlSchemaSubProp="";
+	String yamlSchemaSubProp = "";
 	static String subPropStr = "";
-	static StringBuffer schema = new StringBuffer("\"SCHEMAS\":{");
+	static StringBuffer schema = new StringBuffer("{\"SCHEMAS\":{");
 	static StringBuffer schemaSubProp = new StringBuffer("{");
-	boolean flag = false;
-	
+	static boolean flag = false;
+//	static boolean flag2 = false;
+	static Map<String,Boolean> flag2 = new HashMap<String,Boolean>();
+
 	@GetMapping(value = "v1/test")
 	public String getEmployeeDtls() throws IOException {
 
-		String filePath = "D:\\Project\\JsonObject.txt";
-
+		String filePath = "C:\\Ranjith\\Projects\\samplse.json";
 		String jsonContent = new String(Files.readAllBytes(Paths.get(filePath)));
 
+		// Object Mapper
 		ObjectMapper objMapper = new ObjectMapper();
-
 		JsonNode json = objMapper.readTree(jsonContent);
 
-		
-		StringBuffer schemaProp = new StringBuffer("\"REQUEST\":{\"type\":\"object\",\"properties\":{");
-		json.fields().forEachRemaining(entry -> {
+		StringBuffer ReqResStr = new StringBuffer("\"REQUEST\":{\"type\":\"object\",\"properties\":{");
+
+		// main iteration for request response json
+		json.fields().forEachRemaining(key -> {
+
 			flag = false;
-			String fieldName = entry.getKey();
-			String type = entry.getValue().getNodeType().toString();
-			JsonNode fieldValue = entry.getValue();
-			
-			if(type.equalsIgnoreCase("OBJECT")) {
+			String keyName = key.getKey();
+			String keyType = key.getValue().getNodeType().toString();
+			JsonNode keyValue = key.getValue();
+
+			if (keyType.equalsIgnoreCase("OBJECT")) {
 				flag = true;
-				TestController.generateSubJson(fieldValue,fieldName);
+				TestController.generateSubJson(keyValue, keyName);
 			}
-			
-            String propStr = "\"" + fieldName + "\":{\"type\":\"" + ( flag ? "array" :type.toLowerCase() ) + "\"," +  ( flag ? "\"items\": $ref :"  : "\"enum\":" )
-					+ ( flag ? "\"#/components/schemas/"+fieldName+"Dtls\"" : "["+fieldValue+"]" ) + "}";
-            schemaProp.append(propStr + ",");
-			
+
+			String str = "\"" + keyName + "\":{\"type\":\"" + (flag ? "array" : "string") + "\","
+					+ (flag ? "\"items\": {\"$ref\":" : "\"enum\":")
+					+ (flag ? "\"#/components/schemas/" + keyName + "Dtls\"}" : "[" + keyValue + "]") + "}";
+
+			ReqResStr.append(str + ",");
+
 		});
-		
-		schemaProp.replace(schemaProp.length() - 1, schemaProp.length(), "}}");
-		schema.append(schemaProp+"}");
-		
+
+		// Replace the last comma with close brace
+		ReqResStr.replace(ReqResStr.length() - 1, ReqResStr.length(), "}}");
+
+		// Adding object to schema
+		schema.append(ReqResStr + "}}");
+
 		System.out.println(schema);
-		
+
 		try {
 			ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
 			JsonNode jsonschemaProp = objMapper.readTree(schema.toString());
 			yamlSchemaProp = yamlMapper.writeValueAsString(jsonschemaProp);
-			
+
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
-		System.out.println(yamlSchemaSubProp);
-		return yamlSchemaProp + " "+ yamlSchemaSubProp ;
+
+		return yamlSchemaProp + " " + yamlSchemaSubProp;
 	}
-	
-	public static void generateSubJson(JsonNode fieldValue, String fieldName ) {
-		
-		StringBuffer schemaProp = new StringBuffer("\""+fieldName+"Dtls\":{\"type\":\"object\",\"properties\":{");
-		fieldValue.fields().forEachRemaining(entry2 -> {
-    		String fieldName2 = entry2.getKey();
-			String type2 = entry2.getValue().getNodeType().toString();
-			JsonNode fieldValue2 = entry2.getValue();
+
+	public static void generateSubJson(JsonNode jsonObject, String jsonKey) {
+
+		StringBuffer ReqResStr = new StringBuffer("\"" + jsonKey + "Dtls\":{\"type\":\"object\",\"properties\":{");
+
+		jsonObject.fields().forEachRemaining(key -> {
 			
-			String subPropStr = "\"" + fieldName2 + "\":{\"type\":\"" + type2.toLowerCase() + "\"," + "\"enum\":" + "["
-					+ fieldValue2 + "]}";
-			schemaProp.append(subPropStr + ",");
+			String keyName = key.getKey();
+			String keyType = key.getValue().getNodeType().toString();
+			JsonNode keyValue = key.getValue();
+			flag2.put(keyName,false);
 			
-    	});
-		schemaProp.replace(schemaProp.length() - 1, schemaProp.length(), "}}");
-		schema.append(schemaProp+",");
-		System.out.println("sub schema "+schemaProp);
+			if (keyType.equalsIgnoreCase("OBJECT")) {
+				
+				flag2.replace(keyName, true);
+				
+				TestController.generateSubJson(keyValue, keyName);
+			}
+
+			String str = "\"" + keyName + "\":{\"type\":\"" + (Boolean.valueOf(flag2.get(keyName)) ? "array" : "string") + "\","
+					+ (Boolean.valueOf(flag2.get(keyName)) ? "\"items\": {\"$ref\":" : "\"enum\":")
+					+ (Boolean.valueOf(flag2.get(keyName)) ? "\"#/components/schemas/" + keyName + "Dtls\"}" : "[" + keyValue + "]") + "}";
+
+			ReqResStr.append(str + ",");
+
+		});
+
+		ReqResStr.replace(ReqResStr.length() - 1, ReqResStr.length(), "}}");
+		schema.append(ReqResStr + ",");
 	}
-	
+
 }
